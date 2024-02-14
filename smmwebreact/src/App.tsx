cb3f0cd03d96bb074619257c85ccd99a21385d3a
youssefalb <yousalb891@student.polsl.pl>
2024-02-14 13:04:40 +0100

    Setting up React with Tailwind

---
+++ smmwebreact/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-red-600">
+      Simple React Typescript Tailwind Sample, Nice it works!
+    </h1>
+  );
+}
+
+export default App;
