b5d0256c9808f8db13f4a306115520c3de4f502b
youssefalb <yousalb891@student.polsl.pl>
2024-02-23 00:11:28 +0100

    Refactor ConsultationSection component layout

--- smmwebreact/src/components/ConsultationSection.tsx
+++ smmwebreact/src/components/ConsultationSection.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 const ConsultationSection = () => {
     return (
-        <div className="flex min-h-screen items-center justify-center bg-white">
-            <div className="flex items-center max-w-7xl bg-purple w-3/5 rounded-3xl overflow-hidden">
+        <div className="flex min-h-screen items-center justify-center bg-white relative">
 
-                {/* Left side - Image */}
-                <div className="w-3/10">
+            <div className="flex items-stretch bg-purple w-4/5 rounded-3xl relative overflow-hidden">
+
+                <div className="flex-1 self-stretch w-4/10 ">
                     <img
-                        src="/bench-accounting.png" // Update the src with the path to your image
+                        src="/bench-accounting.png"
                         alt="Person working on a laptop"
-                        className="h-full w-full object-cover"
+                        className="h-full w-full object-center object-cover rounded-l-3xl"
                     />
                 </div>
 
-                {/* Right side - Content */}
-                <div className="w-7/10 p-10">
+                {/* Right side - Content, set a higher z-index to ensure visibility over the decorative image */}
+                <div className="w-7/10 p-10 relative z-20">
                     <h2 className="text-4xl font-bold text-white">
                         Masz jeszcze jakiekolwiek pytania?
                     </h2>
@@ -25,13 +25,20 @@ const ConsultationSection = () => {
                     <button className="bg-black text-white font-bold py-2 px-4 rounded hover:bg-purple-800 transition-colors my-4">
                         Uzyskaj bezpłatną konsultację
                     </button>
-                    <img
-                        src="/vector_yellow.png"
-                        alt="Decorative element"
-                        className="absolute bottom-0 right-0 transform translate-x-1/2 translate-y-1/2"
-                    />
                 </div>
 
+                {/* Yellow decorative element, set a lower z-index to ensure it does not overlap the content */}
+                <img
+                    src="/vector_yellow.png"
+                    alt="Decorative element"
+                    className="absolute bottom-12 right-16 transform translate-x-1/2 translate-y-1/2 z-10"
+                />
+
+                <img
+                    src="/vector_white.png"
+                    alt="Decorative element"
+                    className="absolute top-0 left-0 z-0"
+                />
             </div>
 
         </div>
