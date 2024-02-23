4c4a36002c4a471e6f130d7ae4ce5bcba6cd8a51
Andriy Bobchuk <andriybobchuk@gmail.com>
2024-02-23 21:37:18 +0100

    Create reusable container for all sections containing the common parts and styles; Update Quote/Motto section

---
+++ smmwebreact/src/components/templates/SectionContainer.tsx
@@ -0,0 +1,39 @@
+// Put all the sections you make inside of this container, it contains proper spacing and headers
+
+import React from 'react';
+
+interface SectionContainerProps {
+    title: string;
+    subtitle?: string; // Optional
+    children: React.ReactNode;
+    backgroundColor?: string; // Optional
+    textColor?: string; // Optional
+    subtitleColor?: string; // Optional
+}
+
+const SectionContainer: React.FC<SectionContainerProps> = ({
+    title,
+    subtitle,
+    children,
+    backgroundColor = 'bg-gray-100', // Default background color
+    textColor = 'text-gray-700', // Default main text color
+    subtitleColor = 'text-gray-500', // Default subtitle color
+}) => {
+    return (
+        <div className={`${backgroundColor} py-20`}>
+            <div className="text-center">
+                {subtitle && (
+                    <p className={`uppercase text-sm mb-2 ${subtitleColor}`}>
+                        {subtitle}
+                    </p>
+                )}
+                <h2 className={`text-4xl font-bold mb-20 ${textColor}`}>{title}</h2>
+            </div>
+            <div className={`${textColor}`}>
+                {children} {/* Custom section content */}
+            </div>
+        </div>
+    );
+};
+
+export default SectionContainer;
