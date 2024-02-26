8bc2004998ba053f86c55c1aafa108fe64f76d21
youssefalb <yousalb891@student.polsl.pl>
2024-02-26 19:09:56 +0100

    Changes IDK

--- smmwebreact/src/components/ContactForm.tsx
+++ smmwebreact/src/components/ContactForm.tsx
@@ -27,53 +27,55 @@ const ContactForm = () => {
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-white-100">
-            <div className="bg-gray-100 p-8 rounded-2xl shadow-lg w-full max-w-sm">
-                <h2 className="text-2xl font-bold text-gray-900 mb-4">Contact Us</h2>
-                <p className="text-gray-600 mb-8">Get free consultation for you.</p>
-                <form ref={form} onSubmit={sendEmail}>
-                    <div className="mb-4">
-                        <input
-                            type="text"
-                            name="from_name"
-                            placeholder="Full Name"
-                            className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-                            required
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <input
-                            type="email"
-                            name="from_email"
-                            placeholder="Email Address"
-                            className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-                            required
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <textarea
-                            name="message"
-                            placeholder="Your Message"
-                            className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-                            rows={5}
-                            required
-                        ></textarea>
-                    </div>
-                    <div className="flex items-center justify-between mb-4">
-                        <label className="flex items-center text-gray-600">
-                            <input type="checkbox" className="form-checkbox" />
-                            <span className="ml-2">I agree to the Privacy Policy</span>
-                        </label>
-                        <button
-                            type="submit"
-                            className="px-6 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none"
-                        >
-                            Submit
-                        </button>
-                    </div>
-                </form>
+        <section id="contact">
+            <div className="flex items-center justify-center min-h-screen bg-white-100">
+                <div className="bg-gray-100 p-8 rounded-2xl shadow-lg w-full max-w-sm">
+                    <h2 className="text-2xl font-bold text-gray-900 mb-4">Contact Us</h2>
+                    <p className="text-gray-600 mb-8">Get free consultation for you.</p>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <div className="mb-4">
+                            <input
+                                type="text"
+                                name="from_name"
+                                placeholder="Full Name"
+                                className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                                required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <input
+                                type="email"
+                                name="from_email"
+                                placeholder="Email Address"
+                                className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                                required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <textarea
+                                name="message"
+                                placeholder="Your Message"
+                                className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                                rows={5}
+                                required
+                            ></textarea>
+                        </div>
+                        <div className="flex items-center justify-between mb-4">
+                            <label className="flex items-center text-gray-600">
+                                <input type="checkbox" className="form-checkbox" />
+                                <span className="ml-2">I agree to the Privacy Policy</span>
+                            </label>
+                            <button
+                                type="submit"
+                                className="px-6 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none"
+                            >
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        </section>
     );
 };
 
