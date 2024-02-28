4807637f3617be37cdfdacb28177fe0eb0c10218
Andriy Bobchuk <andriybobchuk@gmail.com>
2024-02-28 21:50:31 +0100

    Updated Footer

--- smmwebreact/src/components/Footer.tsx
+++ smmwebreact/src/components/Footer.tsx
@@ -2,65 +2,39 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-100 text-black p-10">
-            <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-                {/* Company Description */}
+        <footer className="bg-gray-900 text-white p-10 rounded-t-3xl">
+            <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 py-12">
 
-
-                {/* Contact */}
-                {/* <div>
-                    <h3 className="font-bold text-lg mb-2">Contact</h3>
-                    <ul>
-                        <li>Phone</li>
-                        <li>Email</li>
-                        <li>Location</li>
-                        <li>Social Media</li>
-                        <li>Message Form</li>
-                        <li>Customer Center</li>
-                    </ul>
-                </div> */}
-
-                {/* About Us */}
-                {/* <div>
-                    <h3 className="font-bold text-lg mb-2">About Us</h3>
-                    <ul>
-                        <li>Who We Are</li>
-                        <li>Our Team</li>
-                        <li>Careers</li>
-                        <li>Case Studies</li>
-                        <li>Privacy Policy</li>
-                    </ul>
-                </div> */}
                 <div className="">
-                    <h3 className="font-bold text-lg mb-2">Subscribe to Newsletter</h3>
-                    <p className="mb-4">Stay updated on the latest in digital trends and insights.</p>
+                    <h3 className="font-bold text-lg mb-2">Zapisz się na nasz newsletter</h3>
+                    <p className="mb-4 text-gray-400">Zdobądż wiedzę o zarządzaniu obecnością online i zwiększ zainteresowanie Twoją marką.</p>
                     <div className="flex">
                         <input
                             type="text"
-                            placeholder="Enter your email address"
-                            className="w-full px-4 py-2 border rounded-l-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                            placeholder="Adres e-mail"
+                            className="w-full px-4 py-2 border rounded-l-lg text-gray-700 focus:outline-none focus:border-purple"
                         />
-                        <button className="bg-gray-400 p-2 rounded-r">→</button>
+                        <button className="bg-purple p-2 rounded-r">→</button>
                     </div>
                 </div>
                 <div className='ml-24'>
                     <h2 className="font-bold text-lg mb-4">WeboKraft</h2>
-                    <p>WeboKraft is a digital marketing agency based on planet earth established © 2023.</p>
+                    <p className='text-gray-400'>Dynamiczna firma oferująca spersonalizowane rozwiązania, wspierające rozwój biznesu i zwiększające zasięg w internecie.</p>
 
                     {/* Newsletter Subscription */}
 
                 </div>
                 {/* Services */}
                 <div className='ml-24'>
-                    <h3 className="font-bold text-lg mb-2">Sections</h3>
+                    <h3 className="font-bold text-lg mb-2">Menu</h3>
                     <ul>
                         <ul>
-                            <li><a href="#services" className="text-gray-600 hover:text-gray-900">Services</a></li>
-                            <li><a href="#process" className="text-gray-600 hover:text-gray-900">Process</a></li>
-                            <li><a href="#portfolio" className="text-gray-600 hover:text-gray-900">Portfolio</a></li>
-                            <li><a href="#faq" className="text-gray-600 hover:text-gray-900">FAQ</a></li>
-                            <li><a href="#pricing" className="text-gray-600 hover:text-gray-900">Pricing</a></li>
-                            <li><a href="#aboutus" className="text-gray-600 hover:text-gray-900">About us</a></li>
+                            <li><a href="#services" className="text-gray-400 hover:text-purple">Usługi</a></li>
+                            <li><a href="#faq" className="text-gray-400 hover:text-purple">FAQ</a></li>
+                            <li><a href="#portfolio" className="text-gray-400 hover:text-purple">Portfolio</a></li>
+                            <li><a href="#aboutus" className="text-gray-400 hover:text-purple">O Nas</a></li>
+                            <li><a href="#pricing" className="text-gray-400 hover:text-purple">Ceny</a></li>
+                            <li><a href="#contact" className="text-gray-400 hover:text-purple">Kontakt</a></li>
                         </ul>
                     </ul>
                 </div>
