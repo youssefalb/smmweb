5394c245634f60300108b993ed2628ba62ed96e0
Andriy Bobchuk <andriybobchuk@gmail.com>
2024-02-28 21:26:51 +0100

    Updated Contact Section

--- smmwebreact/src/components/ContactForm.tsx
+++ smmwebreact/src/components/ContactForm.tsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import SectionContainer from './templates/SectionContainer';
 
 const ContactForm = () => {
     const form = useRef<HTMLFormElement>(null);
@@ -28,55 +29,68 @@ const ContactForm = () => {
 
     return (
         <section id="contact">
-            <div className="flex items-center justify-center min-h-screen bg-white-100">
-                <div className="bg-gray-100 p-8 rounded-2xl shadow-lg w-full max-w-sm">
-                    <h2 className="text-2xl font-bold text-gray-900 mb-4">Contact Us</h2>
-                    <p className="text-gray-600 mb-8">Get free consultation for you.</p>
-                    <form ref={form} onSubmit={sendEmail}>
-                        <div className="mb-4">
-                            <input
-                                type="text"
-                                name="from_name"
-                                placeholder="Full Name"
-                                className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-                                required
-                            />
+            <SectionContainer
+                title="Masz Jeszcze Pytania?"
+                subtitle="Kontakt"
+            >
+                <div className="bg-white p-12 rounded-2xl m-6">
+                    <h2 className="text-2xl font-bold text-gray-900 mb-4">Napisz do nas! </h2>
+                    
+                    <form ref={form} onSubmit={sendEmail} className="grid grid-cols-1 md:grid-cols-2 gap-12">
+                        <div> {/* First column for name, email, policy, and button */}
+                        <p className="text-gray-600 mb-8">Odpowiemy, oferując darmową konsultację, która pomoże rozwinąć Twój biznes.</p>
+                            <div className="mb-4">
+                                <input
+                                    type="text"
+                                    name="from_name"
+                                    placeholder="Imię i nazwisko"
+                                    className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-purple"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <input
+                                    type="email"
+                                    name="from_email"
+                                    placeholder="Adres e-mail"
+                                    className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-purple"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="flex items-center text-gray-600">
+                                    <input type="checkbox" className="form-checkbox" />
+                                    <span className="ml-2">I agree to the Privacy Policy</span>
+                                </label>
+                            </div>
+                            
                         </div>
-                        <div className="mb-4">
-                            <input
-                                type="email"
-                                name="from_email"
-                                placeholder="Email Address"
-                                className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-                                required
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <textarea
-                                name="message"
-                                placeholder="Your Message"
-                                className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-                                rows={5}
-                                required
-                            ></textarea>
-                        </div>
-                        <div className="flex items-center justify-between mb-4">
-                            <label className="flex items-center text-gray-600">
-                                <input type="checkbox" className="form-checkbox" />
-                                <span className="ml-2">I agree to the Privacy Policy</span>
-                            </label>
+                        <div> {/* Second column for message */}
+                            <div className="mb-4 ">
+                                <textarea
+                                    name="message"
+                                    placeholder="Twoja wiadomość"
+                                    className="w-full  px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-purple"
+                                    rows={9} // Adjust based on your design
+                                    required
+                                ></textarea>
+                            </div>
                             <button
                                 type="submit"
-                                className="px-6 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none"
+                                className="px-6 py-2 text-white bg-purple rounded-lg hover:bg-blue-700 focus:outline-none"
                             >
-                                Submit
+                                Wyślij Wiadomość
                             </button>
                         </div>
                     </form>
                 </div>
-            </div>
+
+            </SectionContainer>
+
         </section>
     );
+
+
 };
 
 export default ContactForm;
