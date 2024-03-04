0039a284b984f54032f707e23501b78737b5e781
youssefalb <yousalb891@student.polsl.pl>
2024-03-04 16:36:25 +0100

    Features animation added

--- smmwebreact/src/components/FeaturesSection.tsx
+++ smmwebreact/src/components/FeaturesSection.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import SectionContainer from './templates/SectionContainer'; // Adjust the import path as necessary
 import { useTranslation } from 'react-i18next';
-
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 interface FeatureCardProps {
     title: string;
     description: string;
     iconPath: string;
-  }
+}
 
-  const FeatureCard: React.FC<FeatureCardProps> = ({ title, description, iconPath }) => {
+const FeatureCard = React.forwardRef<HTMLDivElement, FeatureCardProps>(({ title, description, iconPath }, ref) => {
     return (
-        <div className="bg-white rounded-2xl p-6 shadow-lg flex"> 
-            <div className="flex-shrink-0 rounded-full mr-5"> 
-                <img src={iconPath} alt={title} className="w-14 h-14" /> 
+        <div ref={ref} className="bg-white rounded-2xl p-6 shadow-lg flex">
+            <div className="flex-shrink-0 rounded-full mr-5">
+                <img src={iconPath} alt={title} className="w-14 h-14" />
             </div>
             <div className="flex flex-col">
                 <h3 className="text-xl font-semibold mb-2 text-black">{title}</h3>
@@ -20,11 +22,13 @@ interface FeatureCardProps {
             </div>
         </div>
     );
-};
+});
 
 const FeaturesSection = () => {
     const { t } = useTranslation();
-    
+    const featuresRef = useRef<HTMLDivElement>(null);
+
+
     const features = [
         {
             title: t('package.graphicDesign.title'),
@@ -44,26 +48,59 @@ const FeaturesSection = () => {
         {
             title: t('package.fastLoading.title'),
             description: t('package.fastLoading.description'),
-            icon: '/lad.svg', 
+            icon: '/lad.svg',
         },
         {
             title: t('package.seoOptimization.title'),
             description: t('package.seoOptimization.description'),
-            icon: '/opt.svg', 
+            icon: '/opt.svg',
         },
         {
             title: t('package.trainingAndConsulting.title'),
             description: t('package.trainingAndConsulting.description'),
-            icon: '/szkolenie.svg', 
+            icon: '/szkolenie.svg',
         },
     ];
 
+    useEffect(() => {
+        if (featuresRef.current) {
+            const featureItems = Array.from(featuresRef.current.children);
+
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: featuresRef.current,
+                    start: "top bottom-=100",
+                    end: "bottom top",
+                    toggleActions: "play none play none",
+                    scrub: 1,
+                }
+            })
+                .fromTo(featureItems,
+                    { y: 50, autoAlpha: 0 },
+                    {
+                        y: 0,
+                        autoAlpha: 1,
+                        duration: 3,
+                        stagger: 0.2,
+                        ease: "back.out(1.7)",
+                    }
+                );
+
+            // Cleanup function
+            return () => {
+                if (tl.scrollTrigger) {
+                    tl.scrollTrigger.kill();
+                }
+                tl.kill();
+            };
+        }
+    }, [t]);
+
     return (
         <SectionContainer title={t('package.title')} subtitle={t('package.subtitle')} backgroundColor="bg-purple" textColor="text-white" subtitleColor="text-gray-200">
-            <div className="px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div ref={featuresRef} className="px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {features.map((feature, index) => (
                     <FeatureCard
-                        key={index}
                         title={feature.title}
                         description={feature.description}
                         iconPath={feature.icon}
