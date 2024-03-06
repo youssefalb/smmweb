51e1b5573d60da3511e422c49f59fdace1df7e34
youssefalb <yousalb891@student.polsl.pl>
2024-03-06 23:25:11 +0100

    FAQ animation

--- smmwebreact/src/components/FaqSection.tsx
+++ smmwebreact/src/components/FaqSection.tsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
-import backgroundImage from '../assets/images/faq.svg';  // Update the path to your actual image
+import React, { useEffect, useRef, useState } from 'react';
+import gsap from 'gsap';
+import backgroundImage from '../assets/images/faq.svg';
 import SectionContainer from './templates/SectionContainer';
 import { useTranslation } from 'react-i18next';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 // Define a type for the FAQ data to ensure type safety
 interface FAQItem {
@@ -11,6 +15,7 @@ interface FAQItem {
 
 const FaqSection: React.FC = () => {
     const { t } = useTranslation();
+    const faqsRef = useRef<(HTMLDivElement | null)[]>([]);
 
     const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
@@ -23,6 +28,28 @@ const FaqSection: React.FC = () => {
         { question: t('faq.questions.mobileResponsive.question'), answer: t('faq.questions.mobileResponsive.answer') },
         { question: t('faq.questions.educationalMaterials.question'), answer: t('faq.questions.educationalMaterials.answer') }
     ];
+    useEffect(() => {
+        faqsRef.current = faqsRef.current.slice(0, faqs.length); // Ensure refs array matches the number of FAQ items
+
+        faqsRef.current.forEach((el, index) => {
+            if (!el) return; // Skip if the ref is not assigned
+
+            gsap.fromTo(el, { y: 20, autoAlpha: 0 }, {
+                scrollTrigger: {
+                    trigger: el,
+                    start: "top bottom-=200", // Adjust to control when the animation starts
+                    end: "top center", // Adjust to control when the animation ends
+                    toggleActions: "play none none reverse", // Ensures animation plays in reverse when scrolling up
+                    markers: false, // Set to true to see start and end points during development
+                },
+                autoAlpha: 1,
+                y: 0,
+                duration: 0.5,
+                ease: "power1.out",
+                stagger: 0.1, // Adjust time between the start of each item's animation
+            });
+        });
+    }, [faqs]); // Dependency array
 
     return (
         <section id="faq">
@@ -34,7 +61,9 @@ const FaqSection: React.FC = () => {
                     {/* FAQ cards section, taking 2/3 of the width but offset to the left */}
                     <div className="w-full lg:w-2/3 lg:-mr-1/3 p-8 z-10">
                         {faqs.map((faq, index) => (
-                            <div key={index} className={`bg-white p-5 rounded-2xl ${index === activeIndex ? 'mb-2' : 'mb-2'}`}>
+                            <div key={index}
+                                ref={(el) => (faqsRef.current[index] = el)}
+                                className={`bg-white p-5 rounded-2xl ${index === activeIndex ? 'mb-2' : 'mb-2'}`}>
                                 <button onClick={() => setActiveIndex(index === activeIndex ? null : index)} className="flex items-center w-full text-left">
                                     <div className={`flex-shrink-0 w-8 h-8 rounded-full flex items-center justify-center ${index === activeIndex ? 'bg-white text-black border-2 border-black' : 'bg-black text-white'}`}>
                                         {index === activeIndex ? '-' : '+'}
