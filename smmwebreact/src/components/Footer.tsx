1bb3c41845af26897fe13350931899237999ead9
youssefalb <yousalb891@student.polsl.pl>
2024-03-08 14:03:55 +0100

    Animations for Footer and Motto

--- smmwebreact/src/components/Footer.tsx
+++ smmwebreact/src/components/Footer.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 const Footer = () => {
     const { t } = useTranslation();
+    const newsletterRef = useRef(null);
+    const aboutRef = useRef(null);
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        const elements = [
+            { ref: newsletterRef.current, x: '-100%', opacity: 0 },
+            { ref: aboutRef.current, y: '100%', opacity: 0 },
+            { ref: menuRef.current, x: '100%', opacity: 0 }
+        ];
+
+        elements.forEach(({ ref, x = 0, y = 0, opacity }) => {
+            if (ref) {
+                gsap.fromTo(ref, { x, y, autoAlpha: opacity }, {
+                    x: 0,
+                    y: 0,
+                    autoAlpha: 1,
+                    duration: 1.5,
+                    ease: 'power3.out',
+                    scrollTrigger: {
+                        trigger: ref,
+                        start: "top bottom+=50",
+                        toggleActions: "play none none none",
+                    }
+                });
+            }
+        });
+    }, []);
 
     return (
         <footer className="bg-gray-900 text-white p-10 rounded-t-3xl">
-            <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 md:space-x-5 lg:grid-cols-3 py-12">
-
-                <div className="">
+            <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-12 gap-4">
+                <div ref={newsletterRef} className="">
                     <h3 className="font-bold text-lg mb-2">{t('footer.newsletter.title')}</h3>
                     <p className="mb-4 text-gray-400">{t('footer.newsletter.description')}</p>
                     <div className="flex">
@@ -17,38 +48,33 @@ const Footer = () => {
                             placeholder={t('footer.newsletter.emailPlaceholder')}
                             className="w-full px-4 py-2 border rounded-l-lg text-gray-700 focus:outline-none focus:border-purple"
                         />
-                        <button className="bg-purple p-2 rounded-r">→</button>
+                        <button className="bg-purple p-2 rounded-r-lg">→</button>
                     </div>
                 </div>
-                <div className='lg:ml-24 mt-10'>
+
+                <div ref={aboutRef} className='mt-10 lg:mt-0 lg:ml-24'>
                     <h2 className="font-bold text-lg mb-4">{t('footer.webokraft.title')}</h2>
                     <p className='text-gray-400'>{t('footer.webokraft.description')}</p>
-
-                    {/* Newsletter Subscription */}
-
                 </div>
-                {/* Services */}
-                <div className='lg:ml-24 mt-10'>
+
+                <div ref={menuRef} className='mt-10 lg:mt-0 lg:ml-24'>
                     <h3 className="font-bold text-lg mb-2">Menu</h3>
                     <ul>
-                        <ul>
-                            <li><a href="#services" className="text-gray-400 hover:text-purple">{t('footer.menu.services')}</a></li>
-                            <li><a href="#faq" className="text-gray-400 hover:text-purple">{t('footer.menu.faq')}</a></li>
-                            <li><a href="#portfolio" className="text-gray-400 hover:text-purple">{t('footer.menu.portfolio')}</a></li>
-                            <li><a href="#aboutus" className="text-gray-400 hover:text-purple">{t('footer.menu.about')}</a></li>
-                            <li><a href="#pricing" className="text-gray-400 hover:text-purple">{t('footer.menu.prices')}</a></li>
-                            <li><a href="#contact" className="text-gray-400 hover:text-purple">{t('footer.menu.contact')}</a></li>
-                        </ul>
+                        <li><a href="#services" className="text-gray-400 hover:text-purple">{t('footer.menu.services')}</a></li>
+                        <li><a href="#faq" className="text-gray-400 hover:text-purple">{t('footer.menu.faq')}</a></li>
+                        <li><a href="#portfolio" className="text-gray-400 hover:text-purple">{t('footer.menu.portfolio')}</a></li>
+                        <li><a href="#aboutus" className="text-gray-400 hover:text-purple">{t('footer.menu.about')}</a></li>
+                        <li><a href="#pricing" className="text-gray-400 hover:text-purple">{t('footer.menu.prices')}</a></li>
+                        <li><a href="#contact" className="text-gray-400 hover:text-purple">{t('footer.menu.contact')}</a></li>
                     </ul>
-                </div>
-
-                {/* Social Media Icons */}
-                {/* <div className="flex justify-center lg:justify-start mt-8 lg:mt-0 ml-24">
+                    {/* Social Media Icons */}
+                    {/* <div className="flex justify-center lg:justify-start mt-8 lg:mt-0 ml-24">
                     <a href="#" className="mr-4">wa</a>
                     <a href="#" className="mr-4">fb</a>
                     <a href="#" className="mr-4">tw</a>
                     <a href="#" className="mr-4">ig</a>
                 </div> */}
+                </div>
             </div>
         </footer>
     );
--- smmwebreact/src/components/Motto.tsx
+++ smmwebreact/src/components/Motto.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import SectionContainer from './templates/SectionContainer';
 import { useTranslation } from 'react-i18next';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
-const CustomSection = () => {
+gsap.registerPlugin(ScrollTrigger);
+
+const CustomSection: React.FC = () => {
     const { t } = useTranslation();
+    const leftRefs = useRef<(HTMLSpanElement | null)[]>([]);
+    const rightRefs = useRef<(HTMLSpanElement | null)[]>([]);
+
+    const animateElement = (element: HTMLSpanElement | null, x: number, trigger: ScrollTrigger.Vars) => {
+        if (element) {
+            gsap.fromTo(element,
+                { autoAlpha: 0, x },
+                {
+                    autoAlpha: 1,
+                    x: 0,
+                    duration: 1.5,
+                    ease: 'power3.out',
+                    scrollTrigger: {
+                        ...trigger,
+                        onEnter: () => gsap.to(element, { autoAlpha: 1, x: 0 }),
+                        onLeave: () => gsap.set(element, { autoAlpha: 0, x }),
+                        onEnterBack: () => gsap.to(element, { autoAlpha: 1, x: 0 }),
+                        onLeaveBack: () => gsap.set(element, { autoAlpha: 0, x }),
+                    },
+                });
+        }
+    };
+
+    useEffect(() => {
+        leftRefs.current.forEach((leftRef) => {
+            animateElement(leftRef, -100, { trigger: leftRef!, start: "top bottom", end: "bottom top", toggleActions: "play none none none", });
+        });
+
+        rightRefs.current.forEach((rightRef) => {
+            animateElement(rightRef, 100, { trigger: rightRef!, start: "top bottom", end: "bottom top", toggleActions: "play none none none", });
+        });
+    }, []);
+
+    // Helper functions to add elements to refs
+    const addToRefs = (el: HTMLSpanElement, side: 'left' | 'right') => {
+        if (el) {
+            if (side === 'left') leftRefs.current.push(el);
+            else rightRefs.current.push(el);
+        }
+    };
+
 
     return (
         <SectionContainer
@@ -11,30 +56,30 @@ const CustomSection = () => {
             subtitle={t('mission.subtitle')}
             backgroundColor="bg-white"
         >
-            <div className="flex flex-col items-center justify-center space-y-2 md:space-y-4 mb-20"> {/* Adjust space between rows */}
-                {/* First Row */}
-                <div className="flex md:flex-row items-center space-y-2 space-x-2 md:space-y-0 md:space-x-4"> {/* Adjust layout and spacing */}
-                    <span className="text-md md:text-4xl  text-center">{t('mission.part1')}</span> {/* Adjust font size */}
-                    <div className="bg-purple rounded-3xl w-8 md:w-32 h-6 md:h-10"></div> {/* Adjust rectangle size */}
-                    <span className="text-md md:text-4xl text-center">{t('mission.part2')}</span> {/* Adjust font size */}
+            <div className="flex flex-col items-center justify-center space-y-2 md:space-y-4 mb-20">
+                {/* Example for one row, replicate for others */}
+                <div className="flex md:flex-row items-center space-y-2 space-x-2 md:space-y-0 md:space-x-4">
+                    <span ref={(el) => addToRefs(el!, 'left')} className="text-md md:text-4xl text-center">{t('mission.part1')}</span>
+                    <div className="bg-purple rounded-3xl w-8 md:w-32 h-6 md:h-10"></div>
+                    <span ref={(el) => addToRefs(el!, 'right')} className="text-md md:text-4xl text-center">{t('mission.part2')}</span>
                 </div>
-
                 {/* Second Row */}
                 <div className="flex md:flex-row items-center space-y-2 space-x-2 md:space-y-0 md:space-x-4"> {/* Adjust layout and spacing */}
-                    <span className="text-md md:text-4xl text-center">{t('mission.part3')}</span> {/* Adjust font size */}
+                    <span ref={(el) => addToRefs(el!, 'left')} className="text-md md:text-4xl text-center">{t('mission.part3')}</span> {/* Adjust font size */}
                     <div className="bg-yellow rounded-3xl w-14 md:w-32 h-6 md:h-10"></div> {/* Adjust rectangle size */}
-                    <span className="text-md md:text-4xl text-center">{t('mission.part4')}</span> {/* Adjust font size */}
+                    <span ref={(el) => addToRefs(el!, 'right')} className="text-md md:text-4xl text-center">{t('mission.part4')}</span> {/* Adjust font size */}
                 </div>
 
                 {/* Third Row */}
                 <div className="flex md:flex-row items-center space-y-2 space-x-2 md:space-y-0 md:space-x-4"> {/* Adjust layout and spacing */}
-                    <span className="text-md md:text-4xl text-center">{t('mission.part5')}</span> {/* Adjust font size */}
+                    <span ref={(el) => addToRefs(el!, 'left')} className="text-md md:text-4xl text-center">{t('mission.part5')}</span> {/* Adjust font size */}
                     <div className="bg-green-500 rounded-3xl w-14 md:w-32 h-6 md:h-10"></div> {/* Adjust rectangle size */}
-                    <span className="text-md md:text-4xl text-center">{t('mission.part6')}</span> {/* Adjust font size */}
-                </div>
-            </div>
+                    <span ref={(el) => addToRefs(el!, 'right')} className="text-md md:text-4xl text-center">{t('mission.part6')}</span> {/* Adjust font size */}
+                </div>            </div>
         </SectionContainer>
     );
 };
 
 export default CustomSection;
+
+
